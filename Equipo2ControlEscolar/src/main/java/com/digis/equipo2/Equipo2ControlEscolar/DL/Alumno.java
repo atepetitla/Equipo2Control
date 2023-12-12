f59f20f0ec30830de1f211dace4ead7efcc4df8a
atepetitla <147755392+atepetitla@users.noreply.github.com>
2023-12-12 14:54:18 -0600

    Add files via upload
    Primer push

---
+++ Equipo2ControlEscolar/src/main/java/com/digis/equipo2/Equipo2ControlEscolar/DL/Alumno.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.digis.equipo2.Equipo2ControlEscolar.DL;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+/**
+ *
+ * @author digis
+ */
+@Entity
+public class Alumno {
+    @Id
+    private int idalumno;
+    
+    
+}
