d0c63dc95b31be983634208dd96b2475b1f24753
digis <digis@ALIEN11>
2023-12-12 17:36:51 -0600

    proyectoNCapas

---
+++ src/test/java/com/digis/equipo2/Equipo2ControlEscolar/Equipo2ControlEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.digis.equipo2.Equipo2ControlEscolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo2ControlEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo2ControlEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digis/equipo2/Equipo2ControlEscolar/Equipo2ControlEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digis.equipo2.Equipo2ControlEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipo2ControlEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
