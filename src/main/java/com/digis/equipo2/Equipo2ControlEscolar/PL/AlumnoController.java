ff9fbe515e72627858ec2e303fde74e4c87bd10c
AngelTepetitla <digis@ALIEN11>
2023-12-13 15:06:17 -0600

    proyectoNCapas

--- src/main/java/com/digis/equipo2/Equipo2ControlEscolar/PL/AlumnoController.java
+++ src/main/java/com/digis/equipo2/Equipo2ControlEscolar/PL/AlumnoController.java
@@ -4,13 +4,90 @@
  */
 package com.digis.equipo2.Equipo2ControlEscolar.PL;
 
+import com.digis.equipo2.Equipo2ControlEscolar.DL.Alumno;
+import java.util.List;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
  * @author digis
  */
 @Controller
+@RequestMapping("/alumno")
 public class AlumnoController {
-    
+
+    @GetMapping("/getall")
+    public String getall(Model model) {
+        RestTemplate rest = new RestTemplate();
+        String url = "http://localhost:8080/alumnorest/getall";
+        ResponseEntity<List<Alumno>> response = rest.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Alumno>>() {
+        }
+        );
+        List<Alumno> alumnos = response.getBody();
+        model.addAttribute("alumnos", alumnos);
+        return "Inicio";
+    }
+
+    @GetMapping("/getbyid/{id}")
+    public String getbyid(@PathVariable int id, Model model) {
+        RestTemplate rest = new RestTemplate();
+        String url = "http://localhost:8080/alumnorest/getbyid/" + id;
+        Alumno alumno = rest.getForObject(url, Alumno.class);
+
+        model.addAttribute("alumnos", alumno);
+        return "Inicio";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable int id) {
+        RestTemplate rest = new RestTemplate();
+        String url = "http://localhost:8080/alumnorest/delete/" + id;
+        rest.getForObject(url, Alumno.class);
+        return "redirect:/alumno/getall";
+    }
+
+    @GetMapping("/form/{id}")
+    public String formulario(@PathVariable int id, Model model) {
+        if (id != 0) {
+            RestTemplate rest = new RestTemplate();
+            String url = "http://localhost:8080/alumnorest/getbyid/" + id;
+            Alumno alumno = rest.getForObject(url, Alumno.class);
+            model.addAttribute("alumnos", alumno);
+        } else {
+            model.addAttribute("alumnos", new Alumno());
+        }
+        return "Inicio";
+    }
+
+    @PostMapping("/form")
+    public String guardar(@ModelAttribute("alumno") Alumno alumno) {
+        try {
+            RestTemplate rest = new RestTemplate();
+            String url = "http://localhost:8080/alumnorest/add";
+
+            HttpEntity<Alumno> reques = new HttpEntity<>(alumno);
+            rest.postForObject(url, reques, Alumno.class);
+
+        } catch (Exception e) {
+        }
+
+        return "redirect:/alumno/getall";
+    }
+
 }
